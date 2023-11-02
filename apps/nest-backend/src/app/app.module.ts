5514b6a6c9a7b4fa6c5b830f9323a1b2bd5206b0
Sebastien Bouvier <sebastien@kyklo.co>
2023-11-02 14:55:58 +0700

    reference new resource

--- apps/nest-backend/src/app/app.module.ts
+++ apps/nest-backend/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 
+import { HeroesModule } from '../heroes/heroes.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [HeroesModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
